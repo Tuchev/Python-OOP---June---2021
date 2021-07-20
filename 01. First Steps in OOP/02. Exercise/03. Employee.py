85e120e534d0c8c71a27677db8e062ff0d29383b
Георги Тучев <78377593+Tuchev@users.noreply.github.com>
2021-07-20 15:07:44 +0300

    Add files via upload

---
+++ 01. First Steps in OOP/02. Exercise/03. Employee.py
@@ -0,0 +1,25 @@
+MOUNT_IN_YEAR = 12
+
+class Employee:
+    def __init__(self, id, first_name, last_name, salary):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.salary = salary
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_annual_salary(self):
+        result = self.salary * MOUNT_IN_YEAR
+        return result
+
+    def raise_salary(self, amount):
+        self.salary += amount
+        return self.salary
+
+
+employee = Employee(744423129, "John", "Smith", 1000)
+print(employee.get_full_name())
+print(employee.raise_salary(500))
+print(employee.get_annual_salary())
